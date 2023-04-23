2b8c83db688c00e6d6de2faec902cef448f6e113
vahid <verqam@gmail.com>
2023-04-23 21:17:02 +0330

    AddCARD

---
+++ card/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class FlashCard(models.Model):
+    question = models.TextField()
+    answer = models.TextField()
+
+    create_at = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
