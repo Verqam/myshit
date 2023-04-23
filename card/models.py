a2c9089833b8f8af963e3c9fa6b32aec66ae793c
vahid <verqam@gmail.com>
2023-04-23 21:23:37 +0330

    BR

--- card/models.py
+++ card/models.py
@@ -7,3 +7,6 @@ class FlashCard(models.Model):
 
     create_at = models.DateTimeField(auto_now_add=True)
     modified_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.question
