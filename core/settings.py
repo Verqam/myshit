4d224b01869000d3f80c9e37851eb429789bcc67
vahid <verqam@gmail.com>
2023-04-24 08:22:47 +0330

    added-shit

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'card.apps.CardConfig',
+    'rest_framework',
+    'rauth.apps.RauthConfig'
 
 ]
 
@@ -101,6 +103,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
